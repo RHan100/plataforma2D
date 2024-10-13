90aa28fba5845cfb780c0dcfb7cb7442325d3a41
Miguel A. M. <engmiguelmonteiro@gmail.com>
2024-10-12 22:44:10 -0300

    corrigida a física do personagem, implementação de estrutura da fase

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		body.velocity.y = body.JUMP_FORCE
+		body.velocity.y = -body.jump_velocity
 		owner.anim.play("hurt")
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,38 +2,58 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_FORCE = -400.0
+#const JUMP_FORCE = -400.0
+const AIR_FRICTION := 0.7
 
-@onready var damage: AudioStreamPlayer2D = $damage
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
 var is_hurted := false
 var knockback_vector := Vector2.ZERO
 var direction
-@onready var marker_2d: Marker2D = $Marker2D
 
+#handle jump and gravity
+@export var jump_height := 128
+@export var max_time_to_peak := 0.5
+
+var jump_velocity
+var gravity
+var fall_gravity
+
+
+@onready var damage: AudioStreamPlayer2D = $damage
+@onready var marker_2d: Marker2D = $Marker2D
 @onready var animation:= $animated as AnimatedSprite2D
 #@onready var remote_transform := $remote as RemoteTransform2D
 
 signal player_has_died()
 
+func _ready():
+	jump_velocity = 2*jump_height/max_time_to_peak
+	gravity = 2*jump_height/pow(max_time_to_peak,2)
+	fall_gravity = gravity*1.4
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		velocity.x = 0
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_FORCE
+		velocity.y = -jump_velocity
 		is_jumping = true
 	elif is_on_floor():
 		is_jumping = false
+		
+	if velocity.y > 0 or not Input.is_action_pressed("ui_accept"):
+		velocity.y += fall_gravity*delta
+	else:
+		velocity.y += gravity*delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = lerp(velocity.x, direction * SPEED, AIR_FRICTION)
 		animation.scale.x = direction
 		if !is_jumping:
 			animation.play("run")
